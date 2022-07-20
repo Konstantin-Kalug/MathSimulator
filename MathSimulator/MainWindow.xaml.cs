c7ce31f0963e7557c74df7af59b3e361f85e4264
Konstantin_Kalug <nl17nl17@outlook.com>
2022-07-20 11:30:23 +0700

    new windows

--- MathSimulator/MainWindow.xaml.cs
+++ MathSimulator/MainWindow.xaml.cs
@@ -24,5 +24,36 @@ namespace MathSimulator
         {
             InitializeComponent();
         }
+
+        private void settingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            menuGrid.Visibility = Visibility.Collapsed;
+            settingsGrid.Visibility = Visibility.Visible;
+
+        }
+
+        private void buttonMenuChart_Click(object sender, RoutedEventArgs e)
+        {
+            ChartsWindow chartsWindow = new ChartsWindow();
+            chartsWindow.Show();
+        }
+
+        private void buttonMenuGraph_Click(object sender, RoutedEventArgs e)
+        {
+            GraphsWindow graphsWindow = new GraphsWindow();
+            graphsWindow.Show();
+        }
+
+        private void buttonMenuStatistics_Click(object sender, RoutedEventArgs e)
+        {
+            StatisticsWindow statisticsWindow = new StatisticsWindow();
+            statisticsWindow.Show();
+        }
+
+        private void buttonMenuInfo_Click(object sender, RoutedEventArgs e)
+        {
+            InfoWindow infoWindow = new InfoWindow();
+            infoWindow.Show();
+        }
     }
 }
